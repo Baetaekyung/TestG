8e693fa1fae0ff1ccbe8fb6406ffb6983a989678
eocnddlfma <85099835+eocnddlfma@users.noreply.github.com>
2023-12-21 00:50:44 +0900

    ssh 씬에 recorder 추가함

---
+++ Assets/SSH/Script/pastData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pastData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public pastData(Vector3 p,Quaternion r)
+    {
+        position = p;
+        rotation = r;
+    }
+}
