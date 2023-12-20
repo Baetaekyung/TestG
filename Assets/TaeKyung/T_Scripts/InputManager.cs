5be62ee2e9b6ca3c3d99c919aae339e3b60c4297
Baetaekyung <tkwayi2129@gmail.com>
2023-12-20 23:42:17 +0900

    InputManager
    InputManager(초기)

---
+++ Assets/TaeKyung/T_Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class InputManager : MonoBehaviour
+{
+    public Action inputAction;
+
+    private void InputSystem()
+    {
+        if (Input.anyKeyDown)
+        {
+            inputAction?.Invoke();
+        }
+    }
+
+    private void Start()
+    {
+        inputAction += Test;
+    }
+
+    private void Update()
+    {
+        InputSystem();
+    }
+
+    private void Test()
+    {
+        Debug.Log("입력을 받다...");
+    }
+}
