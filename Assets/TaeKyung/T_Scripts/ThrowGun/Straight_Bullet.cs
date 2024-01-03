975a742014f49c864cada5b69ba83db6e6a10da9
eocnddlfma <85099835+eocnddlfma@users.noreply.github.com>
2024-01-03 23:55:37 +0900

    여전히 맵 제작중

--- Assets/TaeKyung/T_Scripts/ThrowGun/Straight_Bullet.cs
+++ Assets/TaeKyung/T_Scripts/ThrowGun/Straight_Bullet.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Straight_Bullet : MonoBehaviour
 {
@@ -11,5 +13,14 @@ public class Straight_Bullet : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         rigid.AddForce(transform.forward * shootForce, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            //가시와 동일한 스크립트
+        }
+        Destroy(gameObject);
     }
 }
