881b6e28325445f0bdb485f304f21cdc4057d715
eocnddlfma <85099835+eocnddlfma@users.noreply.github.com>
2024-01-04 20:28:00 +0900

    신상현

--- Assets/TaeKyung/T_Scripts/Obstacle/SpikeTrap.cs
+++ Assets/TaeKyung/T_Scripts/Obstacle/SpikeTrap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,19 @@ public class SpikeTrap : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerControllerScr>())
+        {
+            string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneName);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.GetComponent<PlayerControllerScr>())
         {
             string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
             UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneName);
