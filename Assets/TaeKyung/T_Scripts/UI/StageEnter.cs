2737fc3a37c65e396e00ff281f24536dae7bb7ff
Baetaekyung <tkwayi2129@gmail.com>
2024-01-04 16:49:14 +0900

    허브 연결

--- Assets/TaeKyung/T_Scripts/UI/StageEnter.cs
+++ Assets/TaeKyung/T_Scripts/UI/StageEnter.cs
@@ -5,12 +5,11 @@ using UnityEngine.EventSystems;
 
 public class StageEnter : MonoBehaviour, IPointerDownHandler
 {
-    [SerializeField] private int chapterNum;
     [SerializeField] private int loadStageNum;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //UnityEngine.SceneManagement.SceneManager.LoadScene(chapterNum.ToString() + loadStageNum.ToString());
-        Debug.Log(chapterNum.ToString() + " - " + loadStageNum.ToString());
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Puzzle"+loadStageNum.ToString());
+        
     }
 }
